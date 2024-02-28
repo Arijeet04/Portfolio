2b13c590009be654232eb9a9cb89b7b933902620
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-02-28 14:02:43 +0530

    Updated my Recent Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,44 +23,11 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              title="Real State Web App(MERN)"
+              description="My Real Estate Management Web Application, powered by the MERN stack, Redux, JWT, and Firebase, revolutionizes property management. Users navigate a user-friendly interface to browse comprehensive property listings with detailed descriptions, images, and amenities. Robust user authentication via JWT ensures secure access, while Redux manages state efficiently."
+              ghLink="https://github.com/Arijeet04/MERNState"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
@@ -69,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Suicide_Detection_Project_Using_ML"
+              description="My solution employs advanced NLP techniques to analyze the language used in student forum posts. This approach allows us to identify patterns indicating emotional turmoil, depression, or thoughts of self-harm, enabling early intervention and support.."
+              ghLink="https://github.com/Arijeet04/Suicide_Detection_Project_Using_ML"
+              
             />
           </Col>
 
@@ -80,11 +47,11 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="FitBot_Your-Personal-Trainer-Chatbot "
+              description="FitBot is an interactive chatbot that serves as your personal gym trainer. It helps you with workout routines, provides exercise recommendations,and even suggests instructional videos for a better understanding of fitness techniques. With 
+              FitBot, you can have a conversation with a knowledgeable and dedicated virtual trainer to get guidance on your fitness goals."
+              ghLink="https://github.com/Arijeet04/FitBot_Your-Personal-Trainer-Chatbot"
+              
             />
           </Col>
         </Row>
